9e9d6bc11c119f8d223cf352cec0f2ded2657567
Chris Ward <chris@chrisgward.com>
2011-12-17 12:24:28 +1100

    Adding a package and class file. Pretty much just a test for the Github webhook

---
+++ src/main/java/com/crumbdev/crumb/Crumb.java
@@ -0,0 +1,11 @@
+package com.crumbdev.crumb;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: chris
+ * Date: 17/12/11
+ * Time: 12:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Crumb {
+}
