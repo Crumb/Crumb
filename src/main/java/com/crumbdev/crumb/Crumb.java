2c243a8025a6c9666a31cb535d76b3a84867039b
Chris Ward <chris@chrisgward.com>
2011-12-20 04:14:10 +1100

    Attempting to make this a runnable Jar file.

--- src/main/java/com/crumbdev/crumb/Crumb.java
+++ src/main/java/com/crumbdev/crumb/Crumb.java
@@ -1,5 +1,7 @@
 package com.crumbdev.crumb;
 
+import java.util.logging.Logger;
+
 /**
  * Created by IntelliJ IDEA.
  * User: chris
@@ -8,4 +10,10 @@ package com.crumbdev.crumb;
  * To change this template use File | Settings | File Templates.
  */
 public class Crumb {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Crumb, version 0.1 starting up");
+    }
+
 }
