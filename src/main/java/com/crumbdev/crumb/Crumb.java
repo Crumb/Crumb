91b0e7b075c1cbf7296fe14b2383ddaf367cdb2f
Chris Ward <chris@Mudkipz.(none)>
2012-02-14 22:47:48 +1100

    Fixing i18n. Adding a better logger class (Sorry, resba!)
    Signed-off-by: Chris Ward <chris@Mudkipz.(none)>

--- src/main/java/com/crumbdev/crumb/Crumb.java
+++ src/main/java/com/crumbdev/crumb/Crumb.java
@@ -4,7 +4,6 @@ package com.crumbdev.crumb;
  * Everyone loves a standard Javadoc. Will fix this up later.
  */
 
-import com.thenetdev.Commons.Debug;
 import com.thenetdev.Commons.Keyboard;
 
 public class Crumb {
@@ -12,12 +11,17 @@ public class Crumb {
     public static void main(String[] args)
     {
         i18n.i18nInit();
-    	int dt = 1; //Debug toggle. 1 for on, 0 for off.
-        Debug.msg("Crumb, version 0.1 starting up",dt);
-        System.out.print("What is your name? ");
+        Logger.init();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                Logger.close();
+            }
+        });
+        Logger.debug("Crumb, version 0.1 starting up");
+        //Logger.debug("Logfile: " + System.getProperty("user.home") + System.getProperty("file.separator") + ".crumb" + System.getProperty("file.separator") + "crumb.log");
+        Logger.info("What is your name? ");
         String name = Keyboard.readString();
-        System.out.println("Hello "+name+"!");
-        
+        Logger.info("Hello "+name+"!");
     }
 
 }
